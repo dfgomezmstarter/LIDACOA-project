e57379f641936ae8c095d4c79b59368800be2e7b
Cesar Garcia <cagarciap@eafit.edu.co>
2020-03-23 16:08:21 -0500

    Corrigiendo errores de Camilo

--- lidacoa/lidacoa/controlador/descargarInformacionControlador.py
+++ lidacoa/lidacoa/controlador/descargarInformacionControlador.py
@@ -1,13 +1,9 @@
 from ..configuracion import *
 
 def descargar(request):
-
-    print("Cesar hpta")
     baseDatos = database.child('bases_Datos').get()
     nombreBaseDatos = []
-
     for i in baseDatos:
         nombreBaseDatos.append(i.val()['nameDataBase'])
-
     print(nombreBaseDatos)
     return render(request,"descargarInformacion.html",{"arregloBasesDatos" : nombreBaseDatos})
