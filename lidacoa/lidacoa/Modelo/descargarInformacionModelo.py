7af2889474ed3382f70d3e80f92f8815894cf974
Cesar Garcia <cagarciap@eafit.edu.co>
2020-03-23 19:51:20 -0500

    Corrigiendo errores de Camilo 2.0

--- lidacoa/lidacoa/Modelo/descargarInformacionModelo.py
+++ lidacoa/lidacoa/Modelo/descargarInformacionModelo.py
@@ -3,35 +3,30 @@ import pandas as pd
 
 
 def descargar(request):
-    aux = request.POST.get('aux')
-    aux = aux[0:len(aux) - 1]
-    aux = aux.split(",")
-    arreglo = []
-    for i in aux:
-        valor = i[2:len(i) - 1]
-        help = request.POST.get(valor)
-        if str(help) == "1":
-            data = {}
-            nombre_BaseDatos = request.POST.get('nombreBaseDatos')
-            fechaInicial = request.POST.get('fechaInicial')
-            fechaFinal = request.POST.get('fechaFinal')
-            formato = request.POST.get('formato')
-            total = request.POST.get('totalReporte')
+    print("entro")
+    data = {}
+    nombre_BaseDatos = request.POST.get('nombreBaseDatos')
+    fechaInicial = request.POST.get('fechaInicial')
+    fechaFinal = request.POST.get('fechaFinal')
+    formato = request.POST.get('formato')
 
-            consultas = database.child('Consulta').get()
-            for i in consultas:
-                if consultas.val()['fechaInicio'] == fechaInicial and consultas.val()['fechaFinal'] == fechaFinal and \
-                        consultas.val()['nombreBaseDatos'] == nombre_BaseDatos and consultas.val()[
-                    'formatoConsulta'] == formato and consultas.val()['totalReporte'] == total:
-                    data = {
-                        'Base_Datos': consultas.val()['nombreBaseDatos'],
-                        'Fecha Inicial': consultas.val()['fechaInicio'],
-                        'Fecha Final': consultas.val()['fechaFinal'],
-                        'Formato': consultas.val()['formatoConsulta'],
-                        'Total': consultas.val()['totalReporte']
-                    }
-            print(data)
-            file_to_Excel(data)
+    print(str(nombre_BaseDatos) + str(fechaInicial) + str(fechaFinal) + str(formato))
+
+    consultas = database.child('Consulta').get()
+    for i in consultas:
+        if i.val()['fechaInicio'] == fechaInicial and i.val()['fechaFinal'] == fechaFinal and i.val()['nombreBaseDatos'] == nombre_BaseDatos and i.val()['formatoConsulta'] == formato:
+            data = {
+                'Base_Datos': i.val()['nombreBaseDatos'],
+                'Fecha Inicial': i.val()['fechaInicio'],
+                'Fecha Final': i.val()['fechaFinal'],
+                'Formato': i.val()['formatoConsulta'],
+                'Total': i.val()['totalReporte']
+            }
+    print(str(data))
+    df = pd.DataFrame(data, columns=['Base_Datos', 'Fecha Inicial', 'Fecha Final', 'Formato', 'Total'])
+    # df['Total'].sum()
+    df.to_excel('Reporte Consultas', sheet_name='registro')
+    #file_to_Excel(data)
 
 
 def file_to_Excel(file):
