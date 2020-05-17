5f7cd5b6c8f373ae4d04bb728a07181c06d4ea11
Daniel Felipe Gómez Martínez <dfgomezm@eafit.edu.co>
2020-05-17 02:09:50 -0500

    Leve correción del código

--- lidacoa/lidacoa/configuracion.py
+++ lidacoa/lidacoa/configuracion.py
@@ -26,7 +26,7 @@ diccionario ={}
 diccionarioFechas = {}
 FinDeMes = {'01':'-31','02':'-28','03':'-31','04':'-30','05':'-31','06':'-30','07':'-31','08':'-31','09':'-30','10':'-31','11':'-30','12':'-31'}
 arregloFaltantes = []
-diccionario['Cual Quier Cosa']=["kbsrjvajsv","nvsbbasbdkbsa"]
+#diccionario['Cual Quier Cosa']=["kbsrjvajsv","nvsbbasbdkbsa"]
 
 def fechaDeConsulta():
     now = datetime.now()
