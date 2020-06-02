f12e131a66834c08ae938b7ccb11a7e22070c1bf
Juan Camilo G <jcguerrera@eafit.edu.co>
2020-06-01 20:59:11 -0500

    Solucion Bug Image/logo.png

--- lidacoa/lidacoa/controlador/SesionControlador.py
+++ lidacoa/lidacoa/controlador/SesionControlador.py
@@ -10,12 +10,12 @@ def signIn(request):
 
 def buscarImagen(request):
     try:
-        with open("templates\logo.png", "rb") as f:
+        with open("templates/logo.png", "rb") as f:
             return HttpResponse(f.read(), content_type="image/jpeg")
     except IOError:
         red = Image.new('RGBA', (1, 1), (255, 0, 0, 0))
         response = HttpResponse(content_type="image/jpeg")
-        red.save(response, "PNG")
+        red.save(response, "JPEG")
         return response
 
 
